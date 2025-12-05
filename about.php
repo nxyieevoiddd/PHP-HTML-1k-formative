bff7e190a7901616bb28f00af0daf6cc5f1a6b00
DeivisStasuls <deivis.stasuls@gmail.com>
2025-12-05 15:21:36 +0200

    new files and update

--- about.php
+++ about.php
@@ -1,1 +1,23 @@
 <!-- your code goes here -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    
+ </body>
+ <?php
+ echo"<h1>About</h1>";
+ $indexedArray =[
+    ["name"=>"Lorem ipsum dolor sit amet."],
+    ["name"=>"Lorem ipsum dolor sit amet, consectetur adipisicing."],
+    ["name"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iusto."]
+ ];
+ foreach ($indexedArray as $value) {
+    echo "<p>$value[name]</p>";
+};
+ ?>
+ </html>
