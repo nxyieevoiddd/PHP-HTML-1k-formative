7247e248328aaf37bb30fd4ee5a2df7c05a63ec0
Olivers <bindris.olivers@gmail.com>
2026-01-16 14:05:43 +0200

    Everything is completed, with no error's

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>aboutot</title>
+    <title>about</title>
  </head>
  <body>
     <?php
