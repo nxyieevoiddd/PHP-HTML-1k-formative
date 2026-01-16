0f582b41a8d7f2ef48362608b6b820b8a43f6875
Olivers <bindris.olivers@gmail.com>
2026-01-16 13:06:34 +0200

    cloned

--- about.php
+++ about.php
@@ -9,17 +9,7 @@
  <body>
     <?php
  echo"<h1>About</h1>";
- $indexedArray =[
-    ["name"=>"Lorem ipsum dolor sit amet."],
-    ["name"=>"Lorem ipsum dolor sit amet, consectetur adipisicing."],
-    ["name"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iusto."]
- ];
- foreach ($indexedArray as $value) {
-    echo "<p>$value[name]</p>";
-};
  ?>
-    <a href="/contact.html">go to contact</a>
-    <p></p>
     <a href="/index.html">go to index</a>
  </body>
  </html>
